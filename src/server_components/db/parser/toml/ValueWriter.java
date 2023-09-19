a1ab7d083552269bde22ddc8d9f688d1e4a054ce
Tadj Cazaubon <Tadj.d.cazaubon@gmail.com>
2023-09-19 05:04:02 +0200

    use my own toml parser instead

--- src/server_components/db/parser/toml/ValueWriter.java
+++
@@ -1,9 +0,0 @@
-package server_components.db.parser.toml;
-
-interface ValueWriter {
-  boolean canWrite(Object value);
-
-  void write(Object value, WriterContext context);
-
-  boolean isPrimitiveType();
-}
